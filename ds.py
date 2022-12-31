b2c32d466c7f6b9e8f5b711ba04e74d0d1ea847f
ARUNODDEVEN <arunbhagat132@gmail.com>
2022-12-31 13:36:50 +0530

    LinearSearch

--- ds.py
+++ ds.py
@@ -1,13 +1,14 @@
 def linear_search(arr, x):
-  for i in range(len(arr)):
-    if arr[i] == x:
-      return i
-  return -1
+    for i in range(len(arr)):
+        if arr[i] == x:
+            return i
+    return -1
 
-arr = [2, 3, 4, 10, 40]
+
+arrr = [2, 3, 4, 10, 40]
 x = 10
-result = linear_search(arr, x)
+result = linear_search(arrr, x)
 if result != -1:
-  print(f"Element {x} is present at index {result}")
+    print(f"Element {x} is present at index {result}")
 else:
-  print("Element is not present in the array")
+    print("Element is not present in the array")
