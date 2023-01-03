6b8ee1ee79fe3c3f2107862e57a957d4ec0c1b04
ARUNODDEVEN <arunbhagat132@gmail.com>
2023-01-03 13:12:53 +0530

    linkedLIst

--- linkedlisttt.py
+++ linkedlisttt.py
@@ -15,6 +15,7 @@ class Linked_List:
 2.insert a element in  last
 3.insert inbetween elements
 4.print
+5.main
 '''))
         if choice == 1:
             if self.head==None:
@@ -64,10 +65,52 @@ class Linked_List:
                while temp != None:
                 print(temp.data)
                 temp = temp.next
-
+        elif choice ==5:
+            main()
         else:
             print('Enter valid statement:')
             exit(0)
+    def delete(self):
+        print('Enter the choie')
+        choic=int(input('''
+ 1.deletion at first
+ 2.deletion at end
+ 3.deletion in between
+ 4.print 
+ 5.Main'''))
+        if choic==1:
+            if self.head==None:
+                print('list is already Empty')
+            else:
+                self.head=self.head.next
+        elif choic==2:
+            if self.head==None:
+                print('List is already Emptyyy')
+            else:
+                temp=self.head
+                temp2=self.head.next
+                while temp2.next!=None:
+                    temp2=temp.next
+                    temp=temp2
+                temp.next=None
+        elif choic==4:
+            if self.head==None:
+              print('list is empty')
+            else:
+
+               temp = self.head
+               while temp != None:
+                print(temp.data)
+                temp = temp.next
+        elif choic==5:
+            main()
+
+        else:
+            print('invalid choice')
+
+
+        
+                
 
    
        
@@ -78,12 +121,20 @@ def main():
     print('Enter your choice:')
     choic= int(input(''' 
 1.insert
+2.deletion
 0.exit
 '''))
     while choic!=0:
-        mylist.createlist()
-        
-    
-    
+         if choic==1:
+            mylist.createlist()
+         elif choic==2:
+            mylist.delete()
+         elif choic==0:
+            exit(0)
+         else:
+            print('invalid choice')
+            
+
+      
 main()
     
